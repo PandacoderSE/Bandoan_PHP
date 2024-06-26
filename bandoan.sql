69f8e5bc0de9d7df13c254fa01e62eb1041712f1
Nguyen Van Manh <nguyenvanmanh2003dl@gmail.com>
2024-06-26 23:17:42 +0700

    Cap Nhat du an PHP by Manh

--- bandoan.sql
+++ bandoan.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th12 05, 2023 lúc 02:47 PM
--- Phiên bản máy phục vụ: 10.4.28-MariaDB
--- Phiên bản PHP: 8.2.4
+-- Thời gian đã tạo: Th6 26, 2024 lúc 06:15 PM
+-- Phiên bản máy phục vụ: 10.4.32-MariaDB
+-- Phiên bản PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `banthucpham`
+-- Cơ sở dữ liệu: `bandoan`
 --
 
 -- --------------------------------------------------------
@@ -40,7 +40,8 @@ INSERT INTO `category_product` (`id`, `cate_name`) VALUES
 (25, 'Đặc sản'),
 (26, 'Hải sản'),
 (27, 'Khai vị'),
-(28, 'Món chính');
+(28, 'Món chính'),
+(29, ' Đồ Uống');
 
 -- --------------------------------------------------------
 
@@ -50,10 +51,10 @@ INSERT INTO `category_product` (`id`, `cate_name`) VALUES
 
 CREATE TABLE `comment` (
   `id` int(11) NOT NULL,
-  `hten` varchar(20) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `noidung` varchar(500) NOT NULL,
-  `ngaycmt` varchar(50) NOT NULL
+  `hten` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `noidung` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `ngaycmt` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
@@ -61,7 +62,9 @@ CREATE TABLE `comment` (
 --
 
 INSERT INTO `comment` (`id`, `hten`, `email`, `noidung`, `ngaycmt`) VALUES
-(7, 'huyen', 'huyenne@gmail.com', 'shop uy tín', '4/12/2023');
+(8, 'Manh Nguyen', 'nguyenvanmanh2003dl@gmail.com', 'Quan Ban Hang Hoi dat nha', '18/6/2024'),
+(9, 'Nguyen Manh', 'manhdz@gmail.com', 'Quán Làm ăn chán', '18/6/2024'),
+(10, 'MaxCoder', 'nguyenvanmanh2003dl@gmail.com', 'Quán cho mình xin 1 cốc nước free', '20/6/2024');
 
 -- --------------------------------------------------------
 
@@ -82,8 +85,7 @@ CREATE TABLE `ordertb` (
 --
 
 INSERT INTO `ordertb` (`id`, `hten`, `sdt`, `sluong`, `ngay`) VALUES
-(0, 'Nhan', '0901016787', 3, '2022-03-05 06:47:00'),
-(0, 'Hoang', '0901016787', 4, '2022-03-05 18:45:00');
+(3, 'Manh Nguyen', '0852608689', 1000, '2024-06-07 14:31:00');
 
 -- --------------------------------------------------------
 
@@ -107,11 +109,18 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id_product`, `name_product`, `code_product`, `regular_price`, `mota`, `cate_id`, `thumbnail`, `date_product`) VALUES
-(61, 'Món ăn', 'SP01', '59000', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 25, 'f11366925ead8740dac733b2a0b1912c.jpg', '2022-03-04'),
-(62, 'Món ăn 1', 'SP02', '59000', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 25, 'f11366925ead8740dac733b2a0b1912c.jpg', '2022-03-04'),
-(63, 'Món ăn 3', 'SP03', '59000', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 26, 'f11366925ead8740dac733b2a0b1912c.jpg', '2022-03-04'),
-(64, 'Món ăn 3', 'SP04', '59000', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 27, 'f11366925ead8740dac733b2a0b1912c.jpg', '2022-03-04'),
-(65, 'Món ăn 5', 'SP05', '59000', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 28, 'f11366925ead8740dac733b2a0b1912c.jpg', '2022-03-04');
+(66, 'Cá Rô Chiên Giòn', 'SP06', '200000', 'Cá rô phi rất nhiều món ăn thơm ngon, hấp dẫn.', 26, 'cachien.jpg', '2024-06-17'),
+(67, 'Cá Tra Hấp Bia', 'SP07', '122000', 'Cá tra hấp bia là món ăn hấp dẫn', 26, 'cahap.jpg', '2024-06-17'),
+(68, 'Bia 333', 'SP08', '15000', 'Thơm ngon chính hãng bia 333. Bia 333 330ml thùng 24 lon chất lượng bảo đảm', 29, 'bia.jpg', '2024-06-17'),
+(69, 'Giả Cầy', 'SP09', '150000', '150000/1kg giả cầy làm từ thịt...', 25, 'GiaCay.jpg', '2024-06-17'),
+(70, 'Cua Hoàng Đế', 'SP10', '5000000', 'Cua Hoàng Đế hấp bia. Chỉ cần 1-2 lon bia ', 26, 'CuaHoangde.jpg', '2024-06-17'),
+(71, 'Gà Rán MAXCHE', 'SP11', '100000', '100000đ/5 đùi gà chiên , rán tẩm sốt cay', 28, 'GaRan.jpg', '2024-06-17'),
+(72, 'Trà Tắc', 'SP12', '10000', 'Trà tắc là loại nước uống được chế biến giữa trà và quả tắc. ', 29, 'traTac.jpg', '2024-06-17'),
+(73, 'Nem Chua ', 'SP13', '50000', 'Nem chua là một món ăn sử dụng thịt lợn, lợi dụng men của lá chuối', 25, 'nem-chua-chuan-thanh-hoa.jpg', '2024-06-17'),
+(74, 'Trà Sữa Bá Vương', 'SP14', '30000', 'Các tín đồ nghiện “cà kê”, “tà tưa” hiện nay.', 29, 'trasua.png', '2024-06-17'),
+(75, 'Rau Muống Xào Tỏi', 'SP15', '50000', 'Rau muống xào tỏi là một món ăn dân giã', 27, 'image2.jpg', '2024-06-17'),
+(76, 'Thịt Bò Hầm', 'SP16', '500000', 'Bò hầm tiêu xanh nóng hổi, thơm lừng. ', 28, 'thitbo.jpg', '2024-06-17'),
+(77, 'Thịt Kho Tàu', 'SP17', '60000', 'Miếng thịt mềm rục màu đỏ au kèm vị bùi của nước dừa xiêm', 28, 'thit-kho-tau.jpg', '2024-06-17');
 
 -- --------------------------------------------------------
 
@@ -132,7 +141,7 @@ CREATE TABLE `tbl_admin` (
 --
 
 INSERT INTO `tbl_admin` (`id_admin`, `username`, `pwad`, `email`, `lever`) VALUES
-(1, 'huyen', 'e10adc3949ba59abbe56e057f20f883e', 'huyen@gmail.com', 0);
+(4, 'ManhNguyen', 'e10adc3949ba59abbe56e057f20f883e', 'nguyenvanmanh2003dl@gmail.com', 0);
 
 -- --------------------------------------------------------
 
@@ -150,13 +159,6 @@ CREATE TABLE `tbl_cart` (
   `userName` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Đang đổ dữ liệu cho bảng `tbl_cart`
---
-
-INSERT INTO `tbl_cart` (`id_cart`, `id_product`, `tensp`, `soluong`, `dongia`, `hinhanhsp`, `userName`) VALUES
-(97, 61, 'Món ăn', 3, 59000, 'f11366925ead8740dac733b2a0b191', 'ngochuyen');
-
 -- --------------------------------------------------------
 
 --
@@ -165,9 +167,9 @@ INSERT INTO `tbl_cart` (`id_cart`, `id_product`, `tensp`, `soluong`, `dongia`, `
 
 CREATE TABLE `tbl_order` (
   `id` int(11) NOT NULL,
-  `ten` varchar(50) NOT NULL,
-  `sdt` varchar(20) NOT NULL,
-  `diachi` varchar(250) NOT NULL,
+  `ten` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `sdt` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `diachi` varchar(250) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `tong` int(11) NOT NULL,
   `tien` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
@@ -177,11 +179,14 @@ CREATE TABLE `tbl_order` (
 --
 
 INSERT INTO `tbl_order` (`id`, `ten`, `sdt`, `diachi`, `tong`, `tien`) VALUES
-(2, 'Nhan', '0901016787', 'asdfasdad', 1, 59000),
 (3, 'Nhan', '0901016787', 'asdasdada', 1, 59000),
 (4, 'Nhan', '0901016787', 'asdasdada', 1, 59000),
 (5, 'nh', '036654458', 'vl', 1, 59000),
-(6, 'ngochuyen', '0384163818', 'V?nh long', 1, 118000);
+(6, 'ngochuyen', '0384163818', 'V?nh long', 1, 118000),
+(7, 'Nguy?n V?n', '0852608689', 'An Tr?ng Th?i B?nh', 2, 1060000),
+(8, 'M?nh', '0852608689', 'H? N?i ', 1, 5000000),
+(9, 'Nguyen Manh', '0852608689', 'H? N?i Viet Nam', 1, 70000),
+(10, 'Nguyễn Mạnh ĐZZ', '0852608689', 'Chợ Nhổn, Hà Nội', 2, 130000);
 
 -- --------------------------------------------------------
 
@@ -201,11 +206,11 @@ CREATE TABLE `tbl_user` (
 --
 
 INSERT INTO `tbl_user` (`id_user`, `username`, `pass`, `email_user`) VALUES
-(3, 'huyen', '123456', 'ngochuyen@gmail.com'),
-(8, 'Nhan', 'd169186c60e1448fc54c37127323b5d5', 'hoangnhan191120@gmail.com'),
 (9, 'Hoang', 'd169186c60e1448fc54c37127323b5d5', 'hoangnhan191120@gmail.com'),
 (10, 'nhuyen', 'e10adc3949ba59abbe56e057f20f883e', 'ht139944@gmail.com'),
-(11, 'ngochuyen', 'e10adc3949ba59abbe56e057f20f883e', 'huyenit200x@gmail.com');
+(11, 'ngochuyen', 'e10adc3949ba59abbe56e057f20f883e', 'huyenit200x@gmail.com'),
+(12, 'manh', 'e10adc3949ba59abbe56e057f20f883e', 'nguyenvanmanh2003dl@gmail.com'),
+(13, 'ManhNguyen', 'e10adc3949ba59abbe56e057f20f883e', 'nguyenvanmanh200dl@gmail.com');
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -221,6 +226,12 @@ ALTER TABLE `category_product`
 -- Chỉ mục cho bảng `comment`
 --
 ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `ordertb`
+--
+ALTER TABLE `ordertb`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -261,43 +272,49 @@ ALTER TABLE `tbl_user`
 -- AUTO_INCREMENT cho bảng `category_product`
 --
 ALTER TABLE `category_product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT cho bảng `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT cho bảng `ordertb`
+--
+ALTER TABLE `ordertb`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT cho bảng `product`
 --
 ALTER TABLE `product`
-  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
+  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
 
 --
 -- AUTO_INCREMENT cho bảng `tbl_admin`
 --
 ALTER TABLE `tbl_admin`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT cho bảng `tbl_cart`
 --
 ALTER TABLE `tbl_cart`
-  MODIFY `id_cart` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98;
+  MODIFY `id_cart` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110;
 
 --
 -- AUTO_INCREMENT cho bảng `tbl_order`
 --
 ALTER TABLE `tbl_order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT cho bảng `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
