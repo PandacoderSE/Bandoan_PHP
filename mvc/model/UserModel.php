deb1abbe639fe551fe72ac39756193971c6a97e3
Nguyen Van Manh <nguyenvanmanh2003dl@gmail.com>
2024-06-26 23:30:09 +0700

    update lai model các lop

---
+++ mvc/model/UserModel.php
@@ -0,0 +1,22 @@
+<?php
+    class UserModel extends DB{
+         //kết nối
+
+        public function get(){
+            $sql = "SELECT * FROM tbl_user";
+            return mysqli_query($this->con,$sql);
+        }
+        public function dtl($id){
+            $sql = "DELETE FROM tbl_user WHERE id_user=$id";
+            $qr=mysqli_query($this->con,$sql);
+            $kq=false;
+            if($qr){
+            $kq= true;
+            }else{
+            $kq=false;
+            }
+            return json_encode($kq);
+            }  
+    
+    }
+    ?>
